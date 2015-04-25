bf1ed4c3a4c306070f0a1e16338c5ab39acf9971
Matthew Boatright <matthew.c.boatright@gmail.com>
2015-04-24 20:21:27 -0400

    GUI Creation.

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ Author: Matthew Boatright
 Project: C- Compiler
 '''
 
+from Tkinter import *
+from ScrolledText import *
+
 def main():
-    print 'Here'
+    print
+
+    master = Tk()
+
+    Label(master, text="First").grid(row=0)
+    Label(master, text="Second").grid(row=1)
+
+    textPad = ScrolledText(master, width=50, height=40)
+    e2 = Entry(master)
+    e2.config(state=DISABLED)
+
+    textPad.grid(row=0, column=1)
+    e2.grid(row=1, column=1)
+
+    master.mainloop()
 
 if __name__ == '__main__':
     main()
