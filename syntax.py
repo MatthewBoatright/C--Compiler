84f9eb8ccf1809ad575afce3fc3affc8a4048187
Matthew Boatright <matthew.c.boatright@gmail.com>
2015-04-30 23:12:54 -0400

    More syntax progress.

--- syntax.py
+++ syntax.py
@@ -43,7 +43,13 @@ def syntax(input):
         node = Node(None, "Bp", d, 'NT')
         currNode.addChild(node)
 
-        empty(node)
+        if isType('TYPE'):
+            if C(node):
+                Bp(node)
+            else:
+                return False
+        else:
+            empty(node)
 
         return True
 
@@ -55,7 +61,7 @@ def syntax(input):
         if E(node):
             if isType('IDENTIFIER'):
                 accept(node)
-                return Dp(node)
+                return Cp(node)
 
         return False
 
@@ -69,11 +75,21 @@ def syntax(input):
             if G(node):
                 if isVal(')'):
                     accept(node)
-                    return True
-        else: return Dp(node)
+                    return J(node)
+        else:
+            return Dp(node)
 
     def D(currNode):
-        pass
+        d = currNode.depth + 1
+        node = Node(None, "D", d, 'NT')
+        currNode.addChild(node)
+
+        if E(node):
+            if isType('IDENTIFIER'):
+                accept(node)
+                return Dp(node)
+
+        return False
 
     def Dp(currNode):
         d = currNode.depth + 1
@@ -110,10 +126,255 @@ def syntax(input):
         node = Node(None, "G", d, 'NT')
         currNode.addChild(node)
 
-        if isType('TYPE'):
+        if isVal('void'):
+            accept(node)
+            return Gp(node)
+        elif isVal('int') or isVal('float'):
+            accept(node)
+            if isType('IDENTIFIER'):
+                accept(node)
+                return Ip(node) and Hp(node)
+
+    def Gp(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "Gp", d, 'NT')
+        currNode.addChild(node)
+
+        if isType('IDENTIFIER'):
+            return Ip(node) and Hp(node)
+        else:
+            empty(node)
+
+        return True
+
+    def Hp(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "Hp", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal(','):
+            accept(node)
+            return I(node) and Hp(node)
+        else:
+            empty(node)
+
+        return True
+
+    def I(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "I", d, 'NT')
+        currNode.addChild(node)
+
+        if E(node):
+            if isType('IDENTIFIER'):
+                accept(node)
+                return Ip(node)
+
+        return False
+
+    def Ip(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "Ip", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal('['):
+            accept(node)
+            if isVal(']'):
+                accept(node)
+                return True
+            else:
+                return False
+        else:
+            empty(node)
+
+        return True
+
+    def J(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "J", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal('{'):
+            accept(node)
+            if K(node) and L(node):
+                if isVal('}'):
+                    accept(node)
+                    return True
+
+        return False
+
+    def K(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "K", d, 'NT')
+        currNode.addChild(node)
+
+        if D(node):
+            return K(node)
+        else:
+            empty(node)
+
+        return True
+
+    def L(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "L", d, 'NT')
+        currNode.addChild(node)
+
+        if M(node):
+            return L(node)
+        else:
+            empty(node)
+
+        return True
+
+    def M(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "M", d, 'NT')
+        currNode.addChild(node)
+
+        if N(node) or J(node) or O(node) or P(node) or Q(node):
+            return True
+
+        return False
+
+    def N(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "N", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal(';'):
             accept(node)
             return True
+        elif R(node):
+            if isVal(';'):
+                accept(node)
+                return True
+
         return False
+
+    def O(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "O", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal('if'):
+            accept(node)
+            if isVal('('):
+                accept(node)
+                if R(node):
+                    if isVal(')'):
+                        accept(node)
+                        return M(node) and Op(node)
+
+        return False
+
+    def Op(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "Op", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal('else'):
+            accept(node)
+            return M(node)
+        else:
+            empty(node)
+
+        return True
+
+    def P(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "P", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal('while'):
+            accept(node)
+            if isVal('('):
+                accept(node)
+                if R(node):
+                    if isVal(')'):
+                        accept(node)
+                        return M(node)
+
+        return False
+
+    def Q(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "Q", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal('return'):
+            accept(node)
+            return Qp(node)
+
+        return False
+
+    def Qp(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "Qp", d, 'NT')
+        currNode.addChild(node)
+
+        if isVal(';'):
+            accept(node)
+            return True
+        elif R(node):
+            if isVal(';'):
+                accept(node)
+                return True
+
+        return False
+
+    def R(currNode):
+        d = currNode.depth + 1
+        node = Node(None, "R", d, 'NT')
+        currNode.addChild(node)
+
+    def Rp(currNode):
+        pass
+
+    def Re(currNode):
+        pass
+
+    def Sp(currNode):
+        pass
+
+    def Tp(currNode):
+        pass
+
+    def U(currNode):
+        pass
+
+    def V(currNode):
+        pass
+
+    def Vp(currNode):
+        pass
+
+    def W(currNode):
+        pass
+
+    def X(currNode):
+        pass
+
+    def Xp(currNode):
+        pass
+
+    def Y(currNode):
+        pass
+
+    def Z(currNode):
+        pass
+
+    def Zp(currNode):
+        pass
+
+    def Beta(currNode):
+        pass
+
+    def Gamma(currNode):
+        pass
+
+    def Gammap(currNode):
+        pass
     # End rules
 
     if A():
